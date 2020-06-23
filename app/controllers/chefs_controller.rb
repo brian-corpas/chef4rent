fc39acdd8e8047cef3fcc9d8ead24bbb8b378cc0
Brian Corpas <briancorpas@gmail.com>
2020-06-23 12:47:00 +0100

    user pundit with chef submission

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -2,8 +2,30 @@ class ChefsController < ApplicationController
 
   def index
     @chefs = Chef.all
-    
+
   def show
     @chef = Chef.find(params[:id])
   end
+
+  def new
+    @chef = Chef.new
+    current_user.chef = true
+  end
+
+  def create
+      @chef = Chef.new(chef_params)
+      authorize @chef
+      if @chef.save
+        redirect_to chef_path(@chef)
+      else
+        render "new"
+      end
+    end
+  end
+
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:name, :description, :category, :location, :price_range)
 end
