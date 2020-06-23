2c8da75108a99762fab414226a8f37db6fffb481
Ana Goncalves <alsgoncalves91@gmail.com>
2020-06-23 16:03:49 +0100

    Show page and Booking Model

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,10 +1,10 @@
 class ChefsController < ApplicationController
-
   def index
     @chefs = Chef.all
   end
 
   def show
     @chef = Chef.find(params[:id])
+    @booking = Booking.new
   end
 end
