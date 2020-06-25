39ad6adeda6b3f671f44e4bcc9b29d6871430bf4
Brian Corpas <briancorpas@gmail.com>
2020-06-25 13:02:46 +0100

    reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = Review.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @booking = Booking.find(params[:booking_id])
+    @review.booking = @booking
+    @review.save
+    redirect_to profile_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description)
+  end
 end
