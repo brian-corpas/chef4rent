cd73c589edb3885be3910c12b96c5b8ce8abb39a
Ana Goncalves <alsgoncalves91@gmail.com>
2020-06-26 16:10:04 +0100

    fix error

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_26_111515) do
 ActiveRecord::Schema.define(version: 2020_06_26_121359) do
 
   # These are extensions that must be enabled in order to support this database
